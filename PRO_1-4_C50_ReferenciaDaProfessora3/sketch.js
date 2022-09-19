8114db26323343c0eefaf50d5a9c6d5557226ee6
Mariane Fumagalli Prado e Sousa <68484075+marianefps@users.noreply.github.com>
2022-09-19 18:17:47 -0300

    Add files via upload

---
+++ PRO_1-4_C50_ReferenciaDaProfessora3/sketch.js
@@ -0,0 +1,44 @@
+var bg,bgImg;
+var player, shooterImg, shooter_shooting;
+
+
+function preload(){
+  
+  
+
+}
+
+function setup() {
+
+  
+  createCanvas(windowWidth,windowHeight);
+
+  //adicione a imagem de fundo
+  
+  
+
+//crie o sprite do jogador
+
+
+
+}
+
+function draw() {
+  background(0); 
+
+
+
+
+  //mova o jogador para cima e para baixo e torne o jogo compatível com dispositivos móveis usando touches (toques)
+
+
+
+//libere as balas e mude a imagem do atirador para a posição de tiro quando a tecla espaço for pressionada
+
+
+//o jogador volta à imagem original quando pararmos de pressionar a tecla espaço
+
+
+drawSprites();
+
+}
